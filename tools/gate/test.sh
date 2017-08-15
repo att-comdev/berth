bceeecedf3564033261cdbab6eeb7bf668e2189c
Chris Wedgwood <cw@f00f.org>
2017-08-15 21:56:12 +0000

    Use overrides (values) to describe VMs
    Previously the VM configuration was embedded in the charts templates;
    this change moves things into values as we iterate over things created
    resources as required.
    
    Change-Id: I75e60bd31ee5debda90a703775215e92480d572c

---
+++ tools/gate/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+NS=berth
+
+#echo
+#pwd
+#ip r
+#echo
+
+helm install --name=berth --debug ./berth --values=examples/cirros-test.yaml --namespace="${NS}"
+
+# give k8s time to do stuffs
+sleep 10
+
+kubectl -n "${NS}" get pods
+
+IP=$(kubectl get pods  -o wide  -n berth -o json | jq -r '.items[].status.podIP')
+
+# wait for pod to come up say something on ssh
+timeout=60
+t=0
+while : ; do
+    if echo "bye" | nc "${IP}" 22 | grep --quiet ^SSH ; then
+	echo "VM up"
+	exit 0
+    fi
+    if [ $t -gt $timeout ] ; then
+	exit 2
+    fi
+    t=$(($t + 5))
+    sleep 5
+done
+exit 3
